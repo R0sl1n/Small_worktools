269c4e4d9642b183bda53af40e8d60958c807e98
R0sl1n <94353880+R0sl1n@users.noreply.github.com>
2024-08-24 13:48:58 +0200

    Add files via upload

---
+++ move_and_delete_logs.ps1
@@ -0,0 +1,20 @@
+﻿$Date = (Get-Date).ToString('ddMMyyyy-HHmmss')
+
+Start-Transcript "C:\test\Logs\log-$Date.txt"
+
+$MovedFiles = Move-Item c:\test\*.txt C:\test2 -Verbose -PassThru | Select -First 5
+Write-Host "Number of files moved: [$(($MovedFiles | Measure).Count)]"
+
+Stop-Transcript
+
+
+# Days to keep c:\test\logs files.
+$LogsRetention = 7
+
+# Make the retention times negative.
+$LogsRetention = $LogsRetention * -7
+
+# Delete the old Logs files.
+$Logs = (Get-ChildItem C:\test\Logs | `
+    ?{$_.LastWriteTime -lt (Get-Date).AddDays($LogsRetention)})
+$Logs | Remove-Item
